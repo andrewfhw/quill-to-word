e88784997463bb5456ae95360878874847d22968
Andrew Gilbert <andrew@scaffld.com>
2021-01-01 10:08:24 -0600

    fix parser

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,3 @@
-import { ParsedQuillDelta } from 'quilljs-parser';
-export declare function generateWord(delta: ParsedQuillDelta): Promise<Blob>;
+import { RawQuillDelta } from 'quilljs-parser';
+export declare function generateWord(delta: RawQuillDelta): Promise<Blob>;
 //# sourceMappingURL=index.d.ts.map
