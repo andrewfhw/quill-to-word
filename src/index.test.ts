fa1131f29ce8a2f1dba3c9684642efa8c86d2188
Andrew Gilbert <andrew@scaffld.com>
2021-01-04 08:33:52 -0600

    test desc

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { generateWord } from './index';
 import { blank_doc_input, blankDocOutput, simple_paragraph_input, simpleParagraphOutput, multi_paragraph_input, multiParagraphOutput, run_formatting, runFormatting, hyperlinks_simple, hyperlinksSimple, header_simple, headerSimple, code_block_simple, codeblockSimple, block_quote_simple, blockquoteSimple, align_simple, alignSimple, bullet_simple, bulletSimple, bullet_formatted, bulletFormatted, ordered_simple, orderedSimple, embeds_simple, embedsSimple, parsed_delta, parsedDelta, parsedDeltaArray } from './test-io';
 
-describe(' testing raw deltas', () => {
+describe('converts quill to docx', () => {
 
     test('blank doc', async () => {
         const input = await generateWord(blank_doc_input);
